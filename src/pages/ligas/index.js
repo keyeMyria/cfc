48989a373f665434c00a4c8a8f792eee738842fd
Everthon <souza.everthon@gmail.com>
2018-03-24 05:39:01 -0300

    commit

--- src/pages/ligas/index.js
+++ src/pages/ligas/index.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
 import {View, Text, TouchableOpacity, AsyncStorage} from 'react-native';
 import {NavigationActions} from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 export default class Ligas extends Component{
+
+    static navigationOptions = {
+        title: 'Organizações',
+        tabBarIcon: ({ tintColor }) => <Icon name='building' size={20} color={tintColor} />
+      }
 
     signOut = async () => {
         await AsyncStorage.clear();
--- src/pages/times/index.js
+++ src/pages/times/index.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Times extends Component{
+    
+    static navigationOptions = {
+        title: 'Organizações',
+        tabBarIcon: ({ tintColor }) => <Icon name='building' size={20} color={tintColor} />
+      }
+
     render(){
         return(
             <View>
